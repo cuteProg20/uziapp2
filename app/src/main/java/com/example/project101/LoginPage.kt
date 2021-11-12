65f30a5ccd263aa024dfc466c5a4ba6aa38de109
cuteProg20 <janeyjackytz@gmail.com>
2021-11-12 18:03:07 +0300

    Second Commit

--- app/src/main/java/com/example/project101/LoginPage.kt
+++ app/src/main/java/com/example/project101/LoginPage.kt
@@ -1,14 +1,27 @@
 package com.example.project101
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import java.net.PasswordAuthentication
+import java.security.KeyStore
 
 class LoginPage: AppCompatActivity() {
 
+lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_page)
+
+        imageView = findViewById(R.id.imageView3)
+
+
+        var email
+
+        email.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_user, 0, 0, 0)
+
     }
+
 
 }
