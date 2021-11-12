48ab9a60714b63f4076d0a38bbfd3a0684cade21
cuteProg20 <janeyjackytz@gmail.com>
2021-11-12 18:41:17 +0300

    Second Resolved 2 Commit

--- app/src/main/java/com/example/project101/LoginPage.kt
+++ app/src/main/java/com/example/project101/LoginPage.kt
@@ -14,6 +14,48 @@ lateinit var imageView: ImageView
         setContentView(R.layout.login_page)
 
         imageView = findViewById(R.id.imageView3)
+
+        email.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_user, 0, 0, 0)
+        password.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_password, 0, 0, 0)
+
+        email.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int)
+            {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int)
+            {
+
+            }
+
+            override fun afterTextChanged(s: Editable)
+            {
+                if (s.length != 0)
+                {
+                    var drawable = resources.getDrawable(R.drawable.ic_user) //Your drawable image
+                    drawable = DrawableCompat.wrap(drawable!!)
+                    DrawableCompat.setTint(drawable, resources.getColor(R.color.colordarkblue)) // Set whatever color you want
+                    DrawableCompat.setTintMode(drawable, PorterDuff.Mode.SRC_IN)
+                    email.setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null)
+                    email.setCompoundDrawablesWithIntrinsicBounds(resources.getDrawable(R.drawable.message),
+                        null, resources.getDrawable(R.drawable.cancel), null)
+                }
+                else if (s.length == 0)
+                {
+                    email.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.message,
+                        0, 0, 0)
+                    var drawable = resources.getDrawable(R.drawable.message) //Your drawable image
+                    drawable = DrawableCompat.wrap(drawable!!)
+                    DrawableCompat.setTint(drawable, resources.getColor(R.color.colorDefault)) // Set whatever color you want
+                    DrawableCompat.setTintMode(drawable, PorterDuff.Mode.SRC_IN)
+                    email.setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null)
+                    email.setCompoundDrawablesWithIntrinsicBounds(
+                        resources.getDrawable(R.drawable.message),
+                        null, null, null
+                    )
+                }
+            }
+        })
     }
 
 }
