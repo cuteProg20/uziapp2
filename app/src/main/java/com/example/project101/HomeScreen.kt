c3b6f9dd825929fcedd9a541e340389ebad55d8b
cuteProg20 <janeyjackytz@gmail.com>
2021-11-23 20:49:02 +0300

    fifth commit v4.5

--- app/src/main/java/com/example/project101/HomeScreen.kt
+++ app/src/main/java/com/example/project101/HomeScreen.kt
@@ -1,14 +1,51 @@
 package com.example.project101
 
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeScreen : AppCompatActivity() {
+
+    private lateinit var currentFragment : Fragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, currentFragment).commit()
 
+        val bottomNav : BottomNavigationView = findViewById(R.id.fragmentContainerView)
 
+        //setup with nav_controller
+        bottomNav.setupWithNavController (navController)
+
+        //setting the nav listener in the bottom navigation
+       bottomNav.setOnNavigationItemReselectedListener { navController}
+    }
+
+    //create navigation listener function
+
+    val navController = BottomNavigationView.OnNavigationItemReselectedListener {
+        when(
+            it.itemId
+        ){
+            R.id.home -> {
+                currentFragment = HomeFragment()
+            }
+
+            R.id.deal -> {
+                currentFragment = DealsFragment()
+            }
+
+            R.id.profile -> {
+                currentFragment = ProfileFragment()
+            }
+        }
+    supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, currentFragment).commit()
+        true
     }
 
 }
+
