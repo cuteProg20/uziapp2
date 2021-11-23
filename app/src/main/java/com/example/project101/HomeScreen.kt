b4219a384f483fa1c1f04888e0c976692ec88f9f
cuteProg20 <janeyjackytz@gmail.com>
2021-11-23 18:42:34 +0300

    fifth commit v4.4

--- app/src/main/java/com/example/project101/HomeScreen.kt
+++ app/src/main/java/com/example/project101/HomeScreen.kt
@@ -1,12 +1,14 @@
 package com.example.project101
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class HomeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
+
+
     }
 
 }
